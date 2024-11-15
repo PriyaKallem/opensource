200e1a136a5efcb84ff03b29e6960fdabcfedf85
PriyaKallem <156820765+PriyaKallem@users.noreply.github.com>
2024-11-15 14:23:49 +0530

    Update Even_Odd_Game.py

--- visaa_prep/Even_Odd_Game.py
+++ visaa_prep/Even_Odd_Game.py
@@ -1,11 +1,6 @@
-a=input().strip()
-s=0
-temp=(int(a))
-for i in range(len(a)):
-    r=temp%10
-    s=s+r
-    temp=temp//10
-if (s%2==0):
+N=input().strip()
+digit_sum=sum(int(digit) for digit in N)
+if digit_sum%2==0:
     print("Vignesh")
 else:
     print("Charan")
