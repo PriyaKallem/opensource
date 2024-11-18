f078cd6a3ea05ae63739d26e371f206c7616ebb3
PriyaKallem <156820765+PriyaKallem@users.noreply.github.com>
2024-11-18 18:16:10 +0530

    Create Diamond_pattern_2.py

---
+++ visaa_prep/Diamond_pattern_2.py
@@ -0,0 +1,11 @@
+def diamond_pattern(N):
+    for i in range(1,N+1):
+        stars='*' *i
+        spaces=' '* (2*(N-i))
+        print(stars+spaces+stars)
+    for i in range(N-1,0,-1):
+        stars='*' * i
+        spaces=' ' * (2 * (N-i))
+        print(stars+ spaces+stars)
+N=int(input())
+diamond_pattern(N)
